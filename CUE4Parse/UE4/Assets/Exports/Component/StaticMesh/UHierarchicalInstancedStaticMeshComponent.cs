5c1bede177dda542b88a4bc30e0fcf8aca9f4634
Jason <135362740+xdjxsonnn@users.noreply.github.com>
2024-04-06 22:28:52 +0200

    Update UHierarchicalInstancedStaticMeshComponent.cs

--- CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/UHierarchicalInstancedStaticMeshComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/UHierarchicalInstancedStaticMeshComponent.cs
@@ -1,87 +1,64 @@
-using System.Diagnostics;
 using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Core.Math;
+using CUE4Parse.UE4.Readers;
 using CUE4Parse.UE4.Versions;
 using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Assets.Exports.Component.StaticMesh
 {
-
-    public class UInstancedStaticMeshComponent : UStaticMeshComponent
+    public class UFoliageInstancedStaticMeshComponent : UHierarchicalInstancedStaticMeshComponent
     {
-        public FInstancedStaticMeshInstanceData[]? PerInstanceSMData;
-        public float[]? PerInstanceSMCustomData;
+    }
+    public class UHierarchicalInstancedStaticMeshComponent : UInstancedStaticMeshComponent
+    {
+        public FClusterNode_DEPRECATED[]? ClusterTree;
 
         public override void Deserialize(FAssetArchive Ar, long validPos)
         {
             base.Deserialize(Ar, validPos);
 
-            /*            if (Ar.Owner?.Provider?.InternalGameName.ToUpper() == "FORTNITEGAME")
-                        {
-                            var read = Ar.Read<uint>();
-                            switch (read)
-                            {
-                                case 1:
-                                    Ar.Position += 60;
-                                    break;
-                                case 53009:
-                                    Ar.Position += 52;
-                                    break;
-                                default:
-                                    Debugger.Break();
-                                    break;
-                            }
-                        }*/
-
-            PerInstanceSMData = Ar.ReadBulkArray(() => new FInstancedStaticMeshInstanceData(Ar));
-            if (FRenderingObjectVersion.Get(Ar) >= FRenderingObjectVersion.Type.PerInstanceCustomData)
-            {
-                PerInstanceSMCustomData = Ar.ReadBulkArray(Ar.Read<float>);
-            }
-
-            var bCooked = false;
-            if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SerializeInstancedStaticMeshRenderData ||
-                FEditorObjectVersion.Get(Ar) >= FEditorObjectVersion.Type.SerializeInstancedStaticMeshRenderData)
-            {
-                bCooked = Ar.ReadBoolean();
-            }
-
-            var bHasSkipSerializationPropertiesData = FFortniteMainBranchObjectVersion.Get(Ar) < FFortniteMainBranchObjectVersion.Type.ISMComponentEditableWhenInheritedSkipSerialization || Ar.ReadBoolean();
-            if (bHasSkipSerializationPropertiesData)
-            {
-                PerInstanceSMData = Ar.ReadBulkArray(() => new FInstancedStaticMeshInstanceData(Ar));
-                if (FRenderingObjectVersion.Get(Ar) >= FRenderingObjectVersion.Type.PerInstanceCustomData)
-                {
-                    PerInstanceSMCustomData = Ar.ReadBulkArray(Ar.Read<float>);
-                }
-            }
-
-            if (bCooked && (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SerializeInstancedStaticMeshRenderData ||
-                            FEditorObjectVersion.Get(Ar) >= FEditorObjectVersion.Type.SerializeInstancedStaticMeshRenderData))
-            {
-                var renderDataSizeBytes = Ar.Read<ulong>();
-                if (renderDataSizeBytes > 0)
-                {
-                    // FStaticMeshInstanceData::Serialize
-                    Ar.Position = validPos;
-                }
-            }
+            ClusterTree = FReleaseObjectVersion.Get(Ar) < FReleaseObjectVersion.Type.HISMCClusterTreeMigration ? Ar.ReadBulkArray(() => new FClusterNode_DEPRECATED(Ar)) : Ar.ReadBulkArray(() => new FClusterNode(Ar));
         }
 
         protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
         {
             base.WriteJson(writer, serializer);
 
-            if (PerInstanceSMData is { Length: > 0 })
-            {
-                writer.WritePropertyName("PerInstanceSMData");
-                serializer.Serialize(writer, PerInstanceSMData);
-            }
+            if (ClusterTree is not { Length: > 0 }) return;
+            writer.WritePropertyName("ClusterTree");
+            serializer.Serialize(writer, ClusterTree);
+        }
+    }
 
-            if (PerInstanceSMCustomData is { Length: > 0 })
-            {
-                writer.WritePropertyName("PerInstanceSMCustomData");
-                serializer.Serialize(writer, PerInstanceSMCustomData);
-            }
+    public class FClusterNode : FClusterNode_DEPRECATED
+    {
+        public FVector MinInstanceScale;
+        public FVector MaxInstanceScale;
+
+        public FClusterNode(FArchive Ar) : base(Ar)
+        {
+            MinInstanceScale = Ar.Read<FVector>();
+            MaxInstanceScale = Ar.Read<FVector>();
+        }
+    }
+
+    public class FClusterNode_DEPRECATED
+    {
+        public FVector BoundMin;
+        public int FirstChild;
+        public FVector BoundMax;
+        public int LastChild;
+        public int FirstInstance;
+        public int LastInstance;
+
+        public FClusterNode_DEPRECATED(FArchive Ar)
+        {
+            BoundMin = Ar.Read<FVector>();
+            FirstChild = Ar.Read<int>();
+            BoundMax = Ar.Read<FVector>();
+            LastChild = Ar.Read<int>();
+            FirstInstance = Ar.Read<int>();
+            LastInstance = Ar.Read<int>();
         }
     }
 }
